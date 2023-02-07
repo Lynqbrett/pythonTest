a92347e92307c66d7c51fb32f98f896c2402cfaa
brett.reynolds-james <brett.reynolds-james@lynqmes.com>
2023-02-07 13:47:57 +0000

    test commit

---
+++ bitcoin.py
@@ -0,0 +1,35 @@
+import json
+import requests
+from win10toast import ToastNotifier
+import time
+
+    t = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=gbp").text
+    t = json.loads(t)
+
+    price = str(t["bitcoin"]["gbp"])
+
+
+    toast = ToastNotifier()
+    toast.show_toast("bitcoin price", price, duration = 5)
+    time.sleep(20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,228 @@
+# my_name = str("brett")
+#
+# my_age = int(17)
+#
+# sum = my_age + 5
+#
+# print(my_name)
+#
+# print(my_age)
+#
+# print(sum)
+#
+# my_name = str("brett reynolds-james")
+#
+# sum = my_age * 2
+#
+# print(my_name)
+# print(sum)
+#
+# my_name = str(input("what is your first name: "))
+#
+# my_age = int(input("what is your age: "))
+#
+# sum = my_age + 5
+#
+# print("this is your name: " + my_name)
+#
+# print("this is your age: ", my_age)
+#
+# print("this is your age + 5: ", sum)
+#
+# my_name = str(input("what is your full name: "))
+#
+# print("your full name is: " + my_name)
+#
+# sum = my_age * 2
+#
+# print("this is your age multiplied by 2: ", sum)
+#
+# print("hello, my name is " + my_name + "and my age is ", sum)
+
+
+# my_string = "hello world!"
+# print(len(my_string))
+#
+# print(my_string[0])
+# print(my_string[11])
+# print(my_string[6:10])
+
+
+
+# my_string = "estio"
+# print(my_string[2])
+#
+# word = "apprenticeship"
+# for i in word:
+#     print(i)
+#
+# stri = "i love python"
+# print(len(stri))
+#
+# check = "drink plenty of water"
+# if "tea" in check:
+#     print("let's have a cup of tea")
+# else:
+#     print("we'll make do with water")
+
+
+# word = "apprenticeship"
+# print(word[10:14])
+#
+# print(word[1:])
+#
+# print(word[-8:-3])
+
+# a = "hello"
+# print(a.upper())
+#
+# print(a.capitalize())
+#
+# b = "NYC"
+# print(b.lower())
+#
+# c = "    Hello everyone    "
+# print(c.strip())
+#
+# print(c.strip("He" + "e!"))
+
+
+# age = 21
+# age1 = 30
+# age2 = 40
+# text = "my ages are {}, {}, {}"
+# print(text.format(age2, age1, age))
+
+#part 2 of 06/02/2023
+
+# hill1 = 200
+# hill2 = 300
+# if hill1 > hill2:
+#     print("hill1 is bigger than hill2")
+# else:
+#     print("hill2 is bigger than hill1")
+
+
+# def night():
+#     hourday= 23
+#     season = "winter"
+#     if hourday > 16 and season == "winter":
+#         return True
+#
+# if night():
+#     print("Tis dark outside")
+#
+# print(10>20)
+
+
+# def night():
+#     hourday= 18
+#     season = "spring"
+#     if hourday > 16 and season == "winter":
+#         return True
+#     elif hourday == 18 and "spring":
+#         print("it is sunny still")
+#
+# if night():
+#     print("Tis dark outside")
+
+
+# def day():
+#     hour = int(input("what is the time of the day: "))
+#     season = str(input("is it spring or winter: "))
+#     if hour > 18 and "spring":
+#         return True
+#     elif hour > 16 and "winter":
+#         return False
+#
+# if day():
+#     print("it is light still")
+#
+# if not day():
+#     print("it is dark")
+#
+
+
+
+# my_name = str(input("what is your first name: "))
+#
+# my_age = int(input("what is your age: "))
+#
+# sum = my_age + 5
+#
+# print("this is your name: " + my_name)
+#
+# print("this is your age: ", my_age)
+#
+# print("this is your age + 5: ", sum)
+#
+# my_name = str(input("what is your full name: "))
+#
+# print("your full name is: " + my_name)
+#
+# sum = my_age * 2
+#
+# print("this is your age multiplied by 2: ", sum)
+
+
+# age = int(input("what is your age: "))
+# fage = int(input("what is your friends age: "))
+#
+# def friend_older():
+#     if age < fage:
+#         return True
+#     else:
+#         return False
+#
+# if friend_older():
+#     print("your friend could be your parent")
+#
+# if not friend_older():
+#     print("you could be your friends ancestor")
+#
+#
+
+
+# torts = ["herman", "sulcata", "galapagos"]
+# money = [1.67, 252.6, 46.73, 5246.6]
+# T = [True, False, False, True]
+#
+# rando = ["tort", 25.8, True, "cat"]
+#
+#
+# numbers = [1, 2, 3, 4, 5]
+# print(len(numbers))
+# print(sum(numbers))
+# print(max(numbers))
+# print(min(numbers))
+# third = numbers[2]
+# first_three = numbers[0:3]
+# numbers.append(6)
+# numbers.insert(0, 0)
+# numbers.remove(2)
+# print(numbers)
+# print(first_three)
+
+
+
+
+# numbers = [3, 4, 2, 1, 5]
+# words = ['cat', 'dog', 'bird', 'ant', 'elephant']
+#
+# numbers.sort()
+# words.sort()
+# print(numbers)
+# print(words)
+#
+# numbers.reverse()
+# words.reverse()
+# print(numbers)
+# print(words)
+
+
+# words = ['cat', 'Dog', 'BIRD', 'ant', 'Elephant']
+# words.sort(key = str.lower)
+# print(words)
+
+#API stuff below
+
